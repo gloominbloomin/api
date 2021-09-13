90d5db1dd6d790948e3828804055c59efd227f27
gloominbloomin <90053276+gloominbloomin@users.noreply.github.com>
2021-09-13 15:11:36 -0600

    added build command in dockerfile

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,8 +1,15 @@
 pipeline {
     agent any
+     environment {
+        timestamp = "${sh(script:'(date +%Y%m%d%H%M%S)', returnStdout: true)}"
+    }
     stages {
-        stage ('Generate Tag') {
-            sh 'tag=node.$(date +%Y%m%d%H%M%S)'
+        stage('Git clone') {
+            steps {
+            git branch: 'master',
+                // credentialsId: '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',
+                url: 'git@github.com:gloominbloomin/api.git'
+            }
         }
         stage ('Node') {
             steps {
@@ -11,8 +18,52 @@ pipeline {
         }
         stage('Build') {
             steps {
-                echo 'Building $tag ...'
-                sh 'docker build -t $tag .'
+                echo "Building  ${env.timestamp}..."
+                sh('docker build -t $timestamp .')
+                echo '$timestamp'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing... TODO!'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo "Deploying Docker Container ${env.timestamp}"
+                sh "docker run -p 5000:8080 ${env.timestamp}"
+            }
+        }
+    }
+}
+
+/*
+pipeline {
+    agent any
+     environment {
+        LS = "${sh(script:'$(date +%Y%m%d%H%M%S)', returnStdout: true)}"
+    }
+    stages {
+        stage ('Generate Tag') {
+            steps {
+                // T = {$sh((script:'tag=node.$(date +%Y%m%d%H%M%S)'))}
+                // sh 'tag=node.$(date +%Y%m%d%H%M%S)'
+                // echo 'Generated tag: ${tag}'
+                echo 'before'
+                echo "${env.LS}"
+                echo 'after'
+            }
+        }
+        stage ('Node') {
+            steps {
+                // echo 'Node? tag: $tag'
+                sh 'node --version'
+            }
+        }
+        stage('Build') {
+            steps {
+                // echo 'Building "$tag" ...'
+                // sh 'docker build -t $tag .'
             }
         }
         stage('Test') {
@@ -22,9 +73,10 @@ pipeline {
         }
         stage('Deploy') {
             steps {
-                echo 'Deploying Docker Container $node_name'
-                sh 'docker run -p 5000:8080 $tag'
+                // echo 'Deploying Docker Container "$node_name"'
+                // sh 'docker run -p 5000:8080 $tag'
             }
         }
     }
 }
+*/
